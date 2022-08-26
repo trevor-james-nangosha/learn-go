219df366393d15d0fcadba7e6061d86ccd439df4
trevor james nangosha <75243400+trevor-james-nangosha@users.noreply.github.com>
2022-08-26 17:46:03 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// writing hello world in go
+// functions are declared using the func keyord
+func main(){ // every go program must have a main function
+  fmt.Println("hello world")
+  var studentName = "nangosha trevor james"
+  const dateOfBirth = 2001
+
+  fmt.Printf("your name is %v and you were born in %v", studentName, dateOfBirth)
+
+}
+
+// variables and constants
+// in go, variables are declared using the var and const keyword 
+// respectively
+
+// data types in go
